af306ff9a1d19513fa7cf5094f5cdcd9919b30c4
HRSHDG8 <hrshdg8@gmail.com>
2022-04-08 15:49:03 -0400

    cs160

---
+++ src/main/java/edu/sdsu/cs160l/Main.java
@@ -0,0 +1,20 @@
+package edu.sdsu.cs160l;
+
+/**
+ * Locate 2 todos in
+ * Registrar
+ * RegistrarTest
+ *
+ * TODO (4 points)
+ * 3) Explain
+ *  a) flatmap
+ *  b) anyMatch
+ *  c) average
+ *  d) sum
+ *  e) max
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
