61636a4c0f83945a4285af66eece9f47bea21bac
HRSHDG8 <hrshdg8@gmail.com>
2022-04-11 08:57:16 -0700

    cs160

--- src/main/java/edu/sdsu/cs160l/advancedhandling/StreamOperation.java
+++ src/main/java/edu/sdsu/cs160l/advancedhandling/StreamOperation.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamOperation {
+public class  StreamOperation {
   private final List<List<Integer>> numbers;
 
   public StreamOperation() {
